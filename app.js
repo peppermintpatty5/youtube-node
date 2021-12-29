3dc2da2d0fd2b4f43ba35408f026fdbd258366c3
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-12-29 10:30:42 -0500

    Remove unused users route

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const logger = require("morgan");
 
 const fileRouter = require("./routes/file");
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -22,7 +21,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/file", fileRouter);
-app.use("/users", usersRouter);
 app.use("/file", express.static(path.join(__dirname, "videos")));
 
 // catch 404 and forward to error handler

3703b901fe9aca432e70397ac8eacf5913df27ed
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-12-29 10:14:11 -0500

    Route for videos directory
    Access to *.info.json files is forbidden

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+const fileRouter = require("./routes/file");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
@@ -20,7 +21,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/file", fileRouter);
 app.use("/users", usersRouter);
+app.use("/file", express.static(path.join(__dirname, "videos")));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {

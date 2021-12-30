aedf9450c12bf7bfc7c84b8943f1140d32817837
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-12-30 00:30:33 -0500

    Fix video upload dates

--- scripts/populate_db.js
+++ scripts/populate_db.js
@@ -7,6 +7,17 @@ const path = require("path");
 
 const Video = require("../models/video");
 
+/**
+ * Add hyphens to a date string such that `YYYYMMDD` becomes `YYYY-MM-DD`. This
+ * function is idempotent; repeated applications have no effect.
+ *
+ * @param {string} date
+ * @returns {string} the date with hyphens
+ */
+function hyphenDate(date) {
+  return date.replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3");
+}
+
 const dir = process.argv[2];
 
 mongoose.connect("mongodb://localhost:27017/youtube");
@@ -16,9 +27,11 @@ fs.readdir(dir).then((files) => {
     files
       .filter((file) => file.endsWith(".info.json"))
       .map((file) =>
-        fs
-          .readFile(path.join(dir, file), "utf8")
-          .then((text) => Video.create(JSON.parse(text)).catch(() => null))
+        fs.readFile(path.join(dir, file), "utf8").then((text) => {
+          const obj = JSON.parse(text);
+          obj.upload_date = hyphenDate(obj.upload_date);
+          return Video.create(obj).catch(() => null);
+        })
       )
   )
     .then((result) => {

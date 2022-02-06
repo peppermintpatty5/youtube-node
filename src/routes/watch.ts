cb77b4a7f68ff4f9e7c6ad51b42160ca13972b6a
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2022-02-06 14:35:03 -0500

    EJS null checking (#11)
    * Null checks on channel pages
    
    * Null checks on video pages

--- src/routes/watch.ts
+++ src/routes/watch.ts
@@ -6,13 +6,25 @@ import { Video } from "../models";
 
 const router = express.Router();
 
+function formatDate(date: Date) {
+  return moment.utc(date).format("MMM D, YYYY");
+}
+
 router.get("/", (req, res, next) => {
   if (typeof req.query.v === "string")
     Video.findByPk(req.query.v).then((video) => {
       if (video !== null)
         res.render("watch", {
-          video,
-          formatDate: (date: string) => moment.utc(date).format("MMM D, YYYY"),
+          video: {
+            description: video.description ?? "",
+            ext: video.ext ?? "",
+            id: video.id,
+            title: video.title ?? "",
+            upload_date: formatDate(
+              video.upload_date ?? new Date("1970-01-01")
+            ),
+            view_count: (video.view_count ?? 0).toLocaleString(),
+          },
         });
       else next(createError(404));
     });

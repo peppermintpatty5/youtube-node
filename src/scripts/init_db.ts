4c5ff9f848e6c827b780849b48a535fa78033707
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2022-11-10 07:42:23 -0500

    Database init script almost works

--- src/scripts/init_db.ts
+++ src/scripts/init_db.ts
@@ -3,7 +3,8 @@
 import fs from "fs";
 import path from "path";
 
-import sequelize, { Video } from "../models";
+import db from "../db";
+import { Channel, initModels, Video } from "../models";
 
 /**
  * Add hyphens to a date string such that `YYYYMMDD` becomes `YYYY-MM-DD`. This
@@ -15,8 +16,8 @@ function hyphenDate(date: string) {
 
 const dir = process.argv[2];
 
-sequelize
-  .sync()
+initModels(db);
+db.sync()
   .then(() => fs.promises.readdir(dir))
   .then((files) => files.filter((file) => file.endsWith(".info.json")))
   .then((files) =>
@@ -27,8 +28,18 @@ sequelize
           .then((text) => JSON.parse(text))
           .then((record) =>
             Video.create({
-              ...record,
-              upload_date: hyphenDate(record.upload_date),
+              id: record.id,
+              title: record.title,
+              description: record.description,
+              uploadDate: hyphenDate(record.upload_date),
+              duration: record.duration,
+              viewCount: record.view_count,
+              thumbnail: record.thumbnail,
+              ext: record.ext,
+            }).then((newVideo) => {
+              Channel.findByPk(record.channel_id).then((channel) => {
+                if (channel !== null) channel.addVideo(newVideo);
+              });
             })
           )
       )
@@ -40,5 +51,5 @@ sequelize
       results.filter((result) => result.status === "fulfilled").length,
       "videos"
     )
-  )
-  .finally(() => sequelize.close());
+  );
+// .finally(() => db.close());

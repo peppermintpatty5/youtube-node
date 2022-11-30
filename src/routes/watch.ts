174e042de4d623455962f2dd87e8b85622b9e96e
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2022-11-30 07:41:12 -0500

    Update watch page

--- src/routes/watch.ts
+++ src/routes/watch.ts
@@ -19,12 +19,12 @@ router.get("/", (req, res, next) => {
         res.render("watch", {
           id: video.id,
           description: video.description ?? "",
-          ext: video.ext ?? "",
           title: video.title ?? "",
           uploadDate: formatDate(video.uploadDate ?? "1970-01-01"),
           viewCount: (video.viewCount ?? 0).toLocaleString(),
           likeCount: video.likeCount ?? 0,
           dislikeCount: video.dislikeCount ?? 0,
+          localVideoPath: video.localVideoPath,
           channel: {
             id: video.channel?.id,
             name: video.channel?.name,

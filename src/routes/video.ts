cc40e1a31eb22d80dd5082bff287f7eb467694d6
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2023-01-15 18:04:04 -0500

    Same critical fix for videos route

--- src/routes/video.ts
+++ src/routes/video.ts
@@ -1,18 +1,22 @@
 import express from "express";
-import createError from "http-errors";
 import path from "path";
 
 import { Video } from "../models";
 
 const router = express.Router();
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", (req, res) => {
   const { id } = req.params;
 
   Video.findByPk(id).then((video) => {
     if (video !== null && video.localVideoPath !== null)
-      res.sendFile(path.join(__dirname, "../../videos", video.localVideoPath));
-    else next(createError(404));
+      res.sendFile(
+        path.join(__dirname, "../../videos", video.localVideoPath),
+        (err) => {
+          if (err) res.sendStatus(404);
+        }
+      );
+    else res.sendStatus(404);
   });
 });
 

e9aa78163603a0b1d08c98c5781bc798a7b26013
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2023-08-24 00:22:09 -0400

    Add basic pager

--- src/routes/channel.ts
+++ src/routes/channel.ts
@@ -31,27 +31,33 @@ router.get("/:id", (req, res, next) => {
     const { page } = req.query;
     const pageNum = page ? Math.max(1, parseInt(page.toString(), 10) || 1) : 1;
 
-    Channel.findByPk(req.params.id).then((channel) => {
-      if (channel !== null)
-        channel
-          .getVideos({
-            order: [["uploadDate", "ASC"]],
-            offset: (pageNum - 1) * pageSize,
-            limit: pageSize,
-          })
-          .then((videos) => {
-            res.render("channel", {
-              name: channel.name ?? "",
-              videos: videos.map((video) => ({
-                duration: formatDuration(video.duration ?? 0),
-                id: video.id,
-                title: video.title ?? "",
-                uploadDate: formatDate(video.uploadDate ?? "1970-01-01"),
-                viewCount: (video.viewCount ?? 0).toLocaleString(),
-              })),
-            });
-          });
-      else next(createError(404));
+    Channel.findByPk(req.params.id).then(async (channel) => {
+      if (channel !== null) {
+        const numVideos = await channel.countVideos();
+        const numPages = Math.ceil(numVideos / pageSize);
+        const videoSection = await channel.getVideos({
+          order: [["uploadDate", "ASC"]],
+          offset: (pageNum - 1) * pageSize,
+          limit: pageSize,
+        });
+
+        res.render("channel", {
+          name: channel.name ?? "",
+          numVideos,
+          videos: videoSection.map((video) => ({
+            duration: formatDuration(video.duration ?? 0),
+            id: video.id,
+            title: video.title ?? "",
+            uploadDate: formatDate(video.uploadDate ?? "1970-01-01"),
+            viewCount: (video.viewCount ?? 0).toLocaleString(),
+          })),
+          pagination: [...Array(numPages).keys()].map((x) => ({
+            active: pageNum === x + 1,
+            href: `?page=${x + 1}`,
+            text: `${x + 1}`,
+          })),
+        });
+      } else next(createError(404));
     });
   } else res.redirect("/");
 });
